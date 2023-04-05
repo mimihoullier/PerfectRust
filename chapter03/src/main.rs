8f1ab0a1df74f1ec4d1f4d56a7e5de97aa096b31
mimihoullier <okomepopon@icloud.com>
2023-04-05 23:36:46 +0900

    3章おわり

--- chapter03/src/main.rs
+++ chapter03/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // variable::shadowing(); // シャドーイング
     // constant::use_constant();// 定数の宣言と利用
     // scope::blcok_and_scope();// ブロックとスコープ
-    // let result = static_val::calc_amount(100); // 静的変数の宣言と利用
+    //let result = static_val::calc_amount(100); // 静的変数の宣言と利用
     // println!("単価:100の税込み金額={}" , result);
-    // static_val::calc_total(100); // ミュータブルな静的変数の利用
+     static_val::calc_total(100); // ミュータブルな静的変数の利用
 }
